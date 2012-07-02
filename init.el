fcba5e576fd24296126640a420abfce729f71707
Nikola Henezi <nikola.henezi@gmail.com>
2012-07-02 15:46:35 +0200

    Added moving trough windows binds.
    * C-x <key> to move trough windows, where key=left,up, right, down.
    * Removed some spaces before comments.

--- init.el
+++ init.el
@@ -1,26 +1,32 @@
-(add-to-list 'load-path "~/.emacs.d/")                ; directory where config is
+(add-to-list 'load-path "~/.emacs.d/")         ; directory where config is
 
 ;; THEMES
-(require 'color-theme)                                ; loads color theme
-(require 'color-theme-tango)                          ; loads tango color theme
-(color-theme-tango)                                   ; sets color theme to tango
+(require 'color-theme)                         ; loads color theme
+(require 'color-theme-tango)                   ; loads tango color theme
+(color-theme-tango)                            ; sets color theme to tango
 
 ;; EDITING OPTIONS
-(custom-set-variables '(standard-indent 2))           ; sets default indent to 2
-(setq-default tab-width 2)                            ; set tab width to 2 for all buffers
-(setq-default indent-tabs-mode nil)                   ; use spaces for indent
-(require 'uniquify)                                   ; requiered for unique names
-(setq uniquify-buffer-name-style 'forward)            ; defines style for unique names
-(setq uniquify-strip-common-suffix t)                 ; stripes slashes for faster switching
-(require 'linum)                                      ; required for line numbering
-(global-linum-mode)                                   ; enables line numbering 
-(put 'dired-find-alternate-file 'disabled nil)        ; dired uses only one buffer
-(menu-bar-mode -1)                                    ; hides menu bar
-(tool-bar-mode -1)                                    ; hides tool bar
-(scroll-bar-mode -1)                                  ; hides scroll bar
-(setq x-select-enable-clipboard t)                    ; copy outside emacs
-(setq make-backup-files nil)                          ; disables backup files
+(custom-set-variables '(standard-indent 2))    ; sets default indent to 2
+(setq-default tab-width 2)                     ; set tab width to 2 for all buffers
+(setq-default indent-tabs-mode nil)            ; use spaces for indent
+(require 'uniquify)                            ; requiered for unique names
+(setq uniquify-buffer-name-style 'forward)     ; defines style for unique names
+(setq uniquify-strip-common-suffix t)          ; stripes slashes for faster switching
+(require 'linum)                               ; required for line numbering
+(global-linum-mode)                            ; enables line numbering 
+(put 'dired-find-alternate-file 'disabled nil) ; dired uses only one buffer
+(menu-bar-mode -1)                             ; hides menu bar
+(tool-bar-mode -1)                             ; hides tool bar
+(scroll-bar-mode -1)                           ; hides scroll bar
+(setq x-select-enable-clipboard t)             ; copy outside emacs
+(setq make-backup-files nil)                   ; disables backup files
 
+
+;; Fast moving trough windows
+(global-set-key (kbd "C-x <up>") 'windmove-up)
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+(global-set-key (kbd "C-x <down>") 'windmove-down)
+(global-set-key (kbd "C-x <left>") 'windmove-left)
 
 ;; C/C++
 (setq c-default-style "bsd"
@@ -28,7 +34,7 @@
 (c-set-offset 'substatement-open 0)
 
 ;; FUNCTIONS
-(defun toggle-fullscreen ()                           ; full screen mode
+(defun toggle-fullscreen ()                    ; full screen mode
   (interactive)
   (set-frame-parameter nil 'fullscreen (if (frame-parameter nil 'fullscreen)
                                            nil
